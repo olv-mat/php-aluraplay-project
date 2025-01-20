3500d29010028120f3c384c4bb090c954d4ed8b9
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-01-20 18:43:56 -0300

    feature: displaying the video cover image on the home screen

--- views/video_listing.php
+++ views/video_listing.php
@@ -4,21 +4,27 @@ require_once __DIR__ . "/start_html.php";
 <ul class="videos__container" alt="videos alura">
     <?php foreach($videos as $video) :?>
         <?php if(str_starts_with($video->getUrl(), "https://")) :?>
-        <li class="videos__item">
-            <iframe width="100%" height="72%" src="<?= $video->getUrl(); ?>"
-                title="YouTube video player" frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen>
-            </iframe>
-            <div class="descricao-video">
-                <img src="img/logo.png" alt="logo canal alura">
-                <h3><?= $video->getTitle(); ?></h3>
-                <div class="acoes-video">
-                    <a href="update-video?id=<?= $video->getId(); ?>">Editar</a>
-                    <a href="delete-video?id=<?= $video->getId(); ?>">Excluir</a>
+            <li class="videos__item">
+            <?php if (!is_null($video->getImagePath())) :?>
+                <a href="<?= $video->getUrl(); ?>" target="_blank" style="width: 100%">
+                    <img src="img/uploads/<?= $video->getImagePath() ?>" alt="">
+                </a>
+            <?php else :?>
+                <iframe width="100%" height="72%" src="<?= $video->getUrl(); ?>"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen>
+                </iframe>
+            <?php endif; ?>
+                <div class="descricao-video">
+                    <img src="img/logo.png" alt="logo canal alura">
+                    <h3><?= $video->getTitle(); ?></h3>
+                    <div class="acoes-video">
+                        <a href="update-video?id=<?= $video->getId(); ?>">Editar</a>
+                        <a href="delete-video?id=<?= $video->getId(); ?>">Excluir</a>
+                    </div>
                 </div>
-            </div>
-        </li>
+            </li>
         <?php endif; ?>
     <?php endforeach; ?>
 </ul>
